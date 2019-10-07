788354ea4c0803953f70d68b98d2bc02603643c2
furyu-araki <araki.taku@furyu.jp>
2019-10-07 15:38:52 +0900

    tutorial3

--- playground/SimpleVertexShader.vert
+++ playground/SimpleVertexShader.vert
@@ -6,9 +6,14 @@
 // 各頂点ごとに処理が走り、そのたびにvertexPosition_modelspaceに頂点座標が入る？
 // in は入力を表す
 layout(location = 0) in vec3 vertexPosition_modelspace;
+uniform mat4 MVP;
 
 void main() {
     // gl_Positionに結果が入る。今回はそのまま頂点座標を渡す
-    gl_Position.xyz = vertexPosition_modelspace;
-    gl_Position.w = 1.0;
+    // gl_Position.xyz = vertexPosition_modelspace;
+    // gl_Position.w = 1.0;
+    
+    // 変換かける
+    vec4 v = vec4(vertexPosition_modelspace, 1); // wを足す。位置ベクトルなので1
+    gl_Position = MVP * v;
 }
--- playground/playground.cpp
+++ playground/playground.cpp
@@ -12,6 +12,9 @@ GLFWwindow* window;
 
 // GLM（3D数学のライブラリ）をインクルード。めっちゃ便利
 #include <glm/glm.hpp>
+// 変形系の行列計算ができる
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
 // namespaceを書いておくと "glm::vec3" と書く代わりに "vec3" と書ける
 using namespace glm;
 
@@ -73,6 +76,21 @@ int main( void )
     // GLSLのプログラムを作成してコンパイルする
     GLuint programID = LoadShaders("SimpleVertexShader.vert", "SimpleFragmentShader.frag");
     
+    // Matrixをシェーダに渡すための準備
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    
+    // 射影行列。45度の画角、アスペクト比4:3、表示範囲0.1~100
+    mat4 Projection = perspective(radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // カメラ行列
+    mat4 View = lookAt(
+                       vec3(4, 3, 3), // カメラの場所
+                       vec3(0, 0, 0), // 原点を見る
+                       vec3(0, 1, 0)  // 頭が上方向
+                       );
+    // モデル行列
+    mat4 Model = mat4(1.0f);
+    mat4 MVP = Projection * View * Model;
+    
     // 3つの頂点を表す3つのベクトル。つないだら三角形になる。
     // Xは右方向が正、Yは上方向が正、Zは手前方向（自分の後ろの方向）が正
     static const GLfloat g_vertex_buffer_data[] = {
@@ -99,6 +117,10 @@ int main( void )
         
         // コンパイルしたシェーダプログラムを利用する
         glUseProgram(programID);
+        
+        // Matrixをシェーダに送る
+        // 2次元の行列だから、[0][0]で行列の最初の要素のアドレスを渡している？
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
 		// index 0 の頂点属性配列（vertex attribute array）を有効化する？意味分かんない。
         glEnableVertexAttribArray(0);
@@ -136,3 +158,54 @@ int main( void )
 	return 0;
 }
 
+void matrix() {
+    // 平行移動
+    mat4 myMatrix = translate(mat4(), vec3(10.0f, 0.0f, 0.0f));
+    vec4 myVector(10.0f, 10.0f, 10.0f, 0.0f);
+    vec4 transformedVector = myMatrix * myVector;
+    
+    // 単位行列。ベクトルに掛けても何も起きない。数字の1的な存在
+    mat4 myIdentityMatrix = mat4(1.0f);
+    
+    // 拡大縮小行列
+    mat4 myScalingMatrix = scale(vec3(2.0f, 2.0f, 2.0f));
+    
+    // 回転行列
+    mat4 myRotatingMatrix = rotate(90.f, vec3(1.0 , 0.0, 0.0));
+    
+    // 拡大縮小して、回転して、平行移動する
+    mat4 allTransformingMatrix = myMatrix * myRotatingMatrix * myScalingMatrix;
+    vec4 allTransformedVector = allTransformingMatrix * myVector;
+}
+
+void ModelViewProjection() {
+    // 最初はModel Coodinates（モデル空間）
+    // モデルを移動する
+    mat4 ModelMatrix = translate(mat4(), vec3(10.0f, 0.0f, 0.0f));
+    // World Coodinates（ワールド空間）になる
+    
+    // 世界を移動する
+    mat4 ViewMatrix = translate(mat4(), vec3(-3.0f, 0.0f, 0.0f));
+    // Camera Coodinates（カメラ空間）になる
+    
+    // カメラを移動する≒世界を移動するのと実質同じこと
+    mat4 CameraMatrix = lookAt(vec3(0.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+    // まだCamera Coodinates（カメラ空間）
+    
+    // 射影行列。z方向を考慮して描ける。x, yが同じ場合、カメラ空間のzが大きいほうが近いので大きく見える。
+    // 要は、PCのディスプレイにどう映すかを表している？
+    // 四角錐台を立方体にするイメージ。（本当はアスペクト比3:4なので直方体）
+    mat4 ProjectionMatrix = perspective(
+                                        radians(90.0f), // 画角？30~90くらいが普通。90だと超ワイド、30だと超ズーム
+                                        4.0f / 3.0f, // アスペクト比
+                                        0.1f, // 近くのクリッピング平面
+                                        100.0f // 遠くのクリッピング平面
+                                        );
+    // Homogeneous Coodinates（同次空間）になる
+    // 同次座標というのはn次元ベクトルをn+1次元ベクトルで表す的な意味があるっぽい。そうすると便利らしい。
+    
+    // すべて合わせる
+    mat4 MVPmatrix = ProjectionMatrix * ViewMatrix * ModelMatrix;
+    vec4 myVertex(10.0f, 10.0f, 10.0f, 1.0f);
+    vec4 transformedVertex = MVPmatrix * myVertex;
+}
